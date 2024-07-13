a893c0e43dc0c97e921559e836b23c9de56459c2
Zara160991 <zarka1691@gmail.com>
2024-07-13 12:32:01 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+
+
+console.log(10 + 20);
+console.log(10 + 20);
+console.log(10 - 5); 
+console.log(15 / 3);
+console.log(10 % 2);
+console.log((10+5) * 2);
+
+console.log(10 > 5);
+console.log(7 < 3);
+console.log(10 >= 9);
+console.log(7 <= 7);
+console.log(10 == "10");
+console.log(10 === "10");
+console.log(!true);
+console.log(!false);
+console.log(10 != "10");
+console.log(10 !== "10");
+
+console.log(10 > 5 && 7 ===10);
+
+
+const x = 5
+
+
+
+
+        
+
